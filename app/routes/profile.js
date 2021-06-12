c76a09d7ad99e11960e5ed699f23aa04570b094c
kelliaUmuhire <kellumuhire@gmail.com>
2021-06-12 12:55:48 +0200

    profile routes

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,8 +1,10 @@
-const UserController = require("../controllers/UserController");
+const ProfileController = require("../controllers/ProfileController");
 const express = require("express");
 const router = express.Router();
 
-router.post("/signup", UserController.signUp);
-router.post("/signin", UserController.signIn);
+router.post("/", ProfileController.newProfile);
+router.get("/", ProfileController.getAll);
+router.get("/:id", ProfileController.getProfile);
+router.delete("/:id", ProfileController.removeProfile);
 
 module.exports = router;
